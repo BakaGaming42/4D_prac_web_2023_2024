9465866b31e076a54d9e21f5b77c27f301e72c0b
BakaGaming42 <kptzdrdusz@gmail.com>
2023-11-03 09:05:56 +0100

    arkusz "sklep" (rozpoczęty)

---
+++ 15.9.2023/arkusz 5 sklep/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Hurtownia szkolna</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Hurtownia z najlepszymi cenami</h1>
+    </header>
+    <main>
+        <section id="leftSec">
+            <h2>Nasze Ceny</h2>
+            <table></table>
+        </section>
+        <section id="midSec">
+            <h2>Koszt zakupów</h2>
+        </section>
+        <section id="rightSec">
+            <h2>Kontakt</h2>
+            <img alt="hurtownia">
+            <p>e-mail: <a href="hurt@poczta2.pl">hurt@poczta2.pl</a></p>
+        </section>
+    </main>
+    <footer>
+        <h4>Witrynę wykonał 00000000000</h4>
+    </footer>
+</body>
+</html>
